40be1d1f674015d0d47ac26c999408c1f8d0d22c
9251 <juanc.zunzun@educa.jcyl.es>
2024-02-06 16:35:56 +0100

    Create Object Mortgage and Create interface BankRepository

--- src/com/iesam/openbank/domain/Transaction.java
+++ src/com/iesam/openbank/domain/Transaction.java
@@ -1,4 +1,33 @@
 package com.iesam.openbank.domain;
 
 public class Transaction {
+    private final String transactionId;
+    private String description;
+    private double amountMovement;
+
+    public Transaction(String transactionId, String description, double amountMovement) {
+        this.transactionId = transactionId;
+        this.description = description;
+        this.amountMovement = amountMovement;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmountMovement() {
+        return amountMovement;
+    }
+
+    public void setAmountMovement(double amountMovement) {
+        this.amountMovement = amountMovement;
+    }
 }
